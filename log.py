f266b73bf9c2174cde6f48892dabdeae22123fcb
Jens Hertfelder <jens.hertfelder@gmail.com>
2017-04-12 10:20:35 +0200

    Show old logs
    if there is no current log and you just type log, you will get the last lines of the previous existing log file

--- log.py
+++ log.py
@@ -2,14 +2,23 @@
 # -*- coding: utf-8 -*-
 from time import localtime
 from time import strftime
+from glob import glob
 import sys
 import os
+
+
+def get_file_len(fname):
+    with open(fname, 'r') as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 
 
 def main(args):
 
     line_length = 80   # max number of symbols to write in each line
     log_dir     = '.'  # log directory
+    show_lines  = 5    # last x lines to show
 
     # create log directory
     if not os.path.isdir:
@@ -44,11 +53,28 @@ def main(args):
         with open(log_file, 'a') as log_out:
             log_out.write(out_text)
 
-    # show the last 5 entries in the current log file
+    # show the last x lines in the current or last existing log file
+    view_file = log_file
+
     if os.path.isfile(log_file):
-        os.system('tail -n 5 {}'.format(log_file))
+        print 'Log of today:'
+    else:
+        # get old log files and sort them by time
+        old_logs = glob(os.path.join(log_dir, '*.log'))
+        old_logs.sort(key=os.path.getmtime)
+
+        if old_logs:
+            print 'Last log found:'
+            view_file = old_logs[-1]
+
+    if os.path.isfile(view_file):
+        # check if there are more than x lines
+        file_len = get_file_len(view_file)
+
+        if file_len > show_lines:
+            os.system('head -n 1 {}'.format(view_file))
+        os.system('tail -n {} {}'.format(show_lines, view_file))
 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
